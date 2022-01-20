89e6f5dfa9db1092e7ec49bfff5cc5982cef15cf
Salar Hafezi <salar@re-work.dev>
2022-01-20 12:09:32 +0330

    Fix: add code to inspect variables

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 require('dotenv').config();
 
-console.log(process.env.TEST_VAR);
-console.log(process.env.VAR_TEST);
+console.log(process.env.TEST_VAR === 'WORKING' ? 'You win' : 'You lose');
+console.log(process.env.VAR_TEST === 'WORKING' ? 'Another win' : 'Another lose');
